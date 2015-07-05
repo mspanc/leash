82d51668d3239e2c4dd53c3a300771402c165dad
Marcin Lewandowski <marcin@saepia.net>
2015-07-05 23:52:30 +0200

    Return json on errors in user info

--- app/controllers/leash/provider/user_info_controller.rb
+++ app/controllers/leash/provider/user_info_controller.rb
@@ -1,11 +1,11 @@
 class Leash::Provider::UserInfoController < Leash::ProviderController
   def info
-    render text: "missing_authorization_header", status: :unauthorized and return unless request.headers["Authorization"]
-    render text: "missing_authorization_bearer", status: :unauthorized and return unless request.headers["Authorization"].start_with? "Bearer "
+    render json: { error: "missing_authorization_header" }, status: :unauthorized and return unless request.headers["Authorization"]
+    render json: { error: "missing_authorization_bearer" }, status: :unauthorized and return unless request.headers["Authorization"].start_with? "Bearer "
 
     access_token_raw = request.headers["Authorization"].split(" ", 2).last
 
-    render text: "invalid_access_token", status: :forbidden and return unless Leash::Provider::AccessToken.valid?(access_token_raw)
+    render json: { error: "unknown_access_token" }, status: :forbidden and return unless Leash::Provider::AccessToken.valid?(access_token_raw)
 
     access_token =  Leash::Provider::AccessToken.find_by_access_token(access_token_raw)
     owner = access_token.owner_instance
